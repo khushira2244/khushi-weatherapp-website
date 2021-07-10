9b2b5fb4166a0214aa6e1fa78df9176179506c8b
Khushi <khushira2244@gmail.com>
2021-07-10 21:35:55 +0530

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('client side javascript loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-1')
+const messageTwo = document.getElementById('message-2')
+//messageOne.textContent = ''
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const location = search.value
+  messageOne.textContent = 'loading....'
+  messageTwo.textContent = ''
+  fetch('/weather?address=' + location)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+        console.log(data.location)
+        console.log(data.forecast)
+      }
+    })
+})
